c11d950f888512c57af097569213be3cacbd949e
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-07-31 12:03:16 -0300

    Squashed commit of the following:
    commit 4e29b702ce24c9958e11d38832dd997912a3e19a
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Wed Jul 30 16:56:51 2025 -0300
    
        Convert train_task_id to int in get_load_data_from_env function
    
    commit 63f4bf003de7570a7d876a13bfe4655c468a9a10
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Wed Jul 30 02:49:35 2025 -0300
    
        Refactor get_load_data_from_env to include model data retrieval
    
    commit b326f256b62381ab9f464019f9aed73ff932ba62
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Wed Jul 30 02:04:43 2025 -0300
    
        Reorder context menu targets in config.json
    
    commit d34455afbc994056c13d6b104de8f0d36d8c1c9c
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Mon Jul 28 22:51:49 2025 -0300
    
        Add function to load data from environment variables in main.py
    
    commit f45cdf4d4b55abb1b8f402320a5c57a9ce52de8c
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Sat Jul 26 03:45:10 2025 -0300
    
        Fix typo in requirements.txt URL
    
    commit 740f355dd5758520da1534e04070134361ac9460
    Author: Nikolai Petukhov <petukhov.niko@gmail.com>
    Date:   Sat Jul 26 03:41:23 2025 -0300
    
        Add 'ecosystem' to context menu target in config.json

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 import dotenv
 import supervisely as sly
+from supervisely._utils import sly_env
 from supervisely.nn.inference.predict_app.predict_app import PredictApp
 
 dotenv.load_dotenv(os.path.expanduser("~/supervisely(dev).env"))
@@ -10,3 +11,27 @@ dotenv.load_dotenv("local.env")
 api = sly.Api()
 predict_app = PredictApp(api)
 app = predict_app.app
+
+def get_load_data_from_env():
+    project_id = sly_env.project_id(raise_not_found=False)
+    dataset_id = sly_env.dataset_id(raise_not_found=False)
+    items_data = {}
+    if dataset_id:
+        items_data = {"dataset_ids": [dataset_id]}
+    elif project_id:
+        items_data = {"project_id": project_id}
+    train_task_id = os.environ.get("modal.state.trainTaskId", None)
+    model_data = None
+    if train_task_id:
+        model_data = {
+            "mode": "custom",
+            "train_task_id": int(train_task_id),
+        }
+    data = {
+        "items": items_data,
+    }
+    if model_data:
+        data["model"] = model_data
+    return data
+
+predict_app.load_from_json(get_load_data_from_env())
